caff0910030679486af5262151405f1f3c23e387
Jason Stathopulos <jstathopulos@gmail.com>
2015-04-13 07:35:48 -0400

    Add Or Display Tasks When User Is Login

--- meteor-todos.js
+++ meteor-todos.js
@@ -29,7 +29,9 @@ if (Meteor.isClient) {
 
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(), // current time
+        owner: Meteor.userId(), // _id of logged in user
+        username: Meteor.user().username // username of logged in user
       });
 
       // Clear form
